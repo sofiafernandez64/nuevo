0255d98cdf09306656999a2439cc363ede60f89d
Alumno <Alumno@EM44706182>
2021-05-31 23:19:27 -0300

    agregadi

---
+++ src/main/java/ar/edu/unju/edm/Hoyes1Application.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hoyes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hoyes1Application.class, args);
+		System.out.println("hola");
+	}
+
+}
